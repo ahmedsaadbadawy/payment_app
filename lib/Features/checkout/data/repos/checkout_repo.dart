fe0195da9796e91da95b478f5d1493e0d96e4880
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-06-26 05:27:38 +0300

    create checkout repo

---
+++ lib/Features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment_app/Features/checkout/data/models/payment_intent_input_model.dart';
+
+import '../../../../core/errors/failures.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel});
+}
+
